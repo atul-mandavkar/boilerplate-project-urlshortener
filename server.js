4f0fb99cbeeb2e809a56540631c2d786e9fb9f9a
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-09-26 22:31:54 +0530

    First try
    Trying to get url from input box to javascript variable
    But it failed

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { response } = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
@@ -13,11 +15,19 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
+/*
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+*/
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post("api/shorturl", (req, res)=>{
+  let txts = req.body.url;
+  res.send(txts)
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
