65dc27f94f2ca7496371eb4e431b9684edfb492d
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-09-27 21:52:03 +0530

    Second success in checking validity of url and getting hostname etc
    Created a function that check validity of entered url ( like http://www.example.com )
    Then to get hostname, protocol, path I used new URL() method. It will give the hostname, path and protocol for that website.
    Then next step to get ip address of that url and that will get by dns.lookup() method. And for that we require dns module package. Lookup method give ip address.
    Next step tomorrow...

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { response } = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -21,13 +21,45 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 */
+let urlResult;
 
 // To get url from input tag
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
+
+// To check url validity as "http://www.example.com"
+function checkValidity(arg){
+  let res = arg.match(/\w+./g);
+  console.log(res[0]);
+  if(res[0] === "http:" || res[0] === "https:" || res[0] === "ftp:"){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
 app.post("/api/shorturl", (req, res)=>{
-  res.json({value: req.body.url});
+  urlResult = req.body.url;
+  checkValidity(urlResult);
+  if(checkValidity(urlResult)){
+    const  domain = new URL(urlResult);
+    console.log("There is a website");
+    console.log(domain.hostname);
+    console.log(domain.pathname);
+    console.log(domain.protocol);
+    // To check url's hostname and address
+    dns.lookup(domain.hostname, (err, address, family)=>{
+      console.log(family);
+      console.log(address);
+    });
+    res.json({original_url: req.body.url});
+  }
+  else{
+    console.log("There is no such website");
+    res.json({error: "Invalid url"});
+  }
 });
+
 
 
 app.listen(port, function() {
