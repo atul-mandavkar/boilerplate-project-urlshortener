e895d43f9316bba42ee3f3dbc792fcb77a4983a6
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-09-30 20:49:45 +0530

    Success in validate url
    Use url module, by requiring in it.,
    By this module we can get if urlString is url then we will get all its object readily by parsing it by parse(urlString).objectName.
    So one of that object , ie, hostname is we get.
    That host name is the first argument for the dns.lookup() method and second argument is callback function which will give either err or address of that url hostname.
    
    Note: -
    Before using dns lookup method use reg expression to check if urlString  has http:// or https:// written or not at starting of urlString because we require url in format like https://www.example.com

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const dns = require("dns");
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const urlparsed = require("url");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,50 +22,27 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 */
-let urlResult;
+let urlResult, message1 = "";
 
 // To get url from input tag
 app.use(bodyParser.urlencoded({extended: false}));
 
 // To check url validity as "http://www.example.com"
-function checkValidity(arg){
-  let newUrl;
-  try{
-    newUrl = new URL(arg);
-  }
-  catch{
-    return false;
-  }
-  console.log(newUrl.host);
-  if((/^http(s)?:\/\//).test(arg)){
-    let fHost1 = newUrl.host;
-    console.log(fHost1.includes("www."));
-    console.log(fHost1.includes(".com"));
-    console.log(fHost1.includes(".co."));
-    console.log(fHost1.match(/\w+\./g));
-    console.log(fHost1.match(/\w+\./g).length);
-    console.log((/^.\w+/g).test(fHost1));
-    console.log(fHost1.match(/^.\w+/g));
-    return true;
-   }
-}
 
 app.post("/api/shorturl", (req, res)=>{
   console.log();
   urlResult = req.body.url;
-  if(checkValidity(urlResult)){
-    console.log("There is a website");
-    /*const  domain = new URL(urlResult);
-    console.log(domain.hostname);
-    // To check url's hostname and address
-    dns.lookup(domain.hostname, (err, address, family)=>{
-      console.log(family);
-      console.log(address);
-    });*/
-    res.json({original_url: req.body.url});
+  if((/^http(s)?:\/\//).test(urlResult)){
+    let result = dns.lookup(urlparsed.parse(urlResult).hostname, (err, address, family)=>{
+      if(address){
+        res.json({original_url: urlResult});
+      }
+      else{
+        res.json({error: "Invalid hostname"});
+      }
+    });
   }
   else{
-    console.log("There is no such website");
     res.json({error: "Invalid url"});
   }
 });
