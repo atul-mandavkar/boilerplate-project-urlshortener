ebdb2c8c2a69e542e806bd6c966c31f6ccc2006e
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-10-01 22:39:03 +0530

    Successs in completing project
    After creating module, I saved first data by instanceName.save() method so that we can do further operations.
    Each time when validity of url is checked,  there is also check that url availability in database by compairing with original_url.
    If available then short_url is taken from database else if not present in database  then created new short_url.
    New short_url is created by adding 1 to last short_url (means maximum short_url number) and we get this last short_url by modelName.find() method
    After that , the last part if the short_url is placed after /api/shorturl then it will tedirected to original_url of that database, which is find by moduleName.findOne() method
    And that's it
    Next time create the same app with replit so we can publically hosted it.

--- server.js
+++ server.js
@@ -23,14 +23,24 @@ app.get('/', function(req, res) {
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // To create schema for database for that first require {Scema} from monhoose module
-const urlSchema = new Schema({
+const UrlSchema = new Schema({
   original_url: {type: String, require: true},
   short_url: Number
 });
 
 // Now creating model in database of this schema
 
-let url_data = mongoose.model("url_data", urlSchema);
+let Url_data = mongoose.model("Url_data", UrlSchema);
+
+// Creating first instance in databasse
+let UrlNew = new Url_data({
+  original_url: "https://www.example.com",
+  short_url: 1
+});
+
+// To save first instance
+//UrlNew.save();
+
 
 /*
 // Your first API endpoint
@@ -43,26 +53,75 @@ let urlResult, message1 = "";
 // To get url from input tag
 app.use(bodyParser.urlencoded({extended: false}));
 
-// To check url validity as "http://www.example.com"
+let indexOfShortUrl;
 
+// To check url validity as "http://www.example.com"
 app.post("/api/shorturl", (req, res)=>{
-  console.log();
   urlResult = req.body.url;
+  // If there is https:// or http:// is present at start of url
   if((/^http(s)?:\/\//).test(urlResult)){
     let result = dns.lookup(urlparsed.parse(urlResult).hostname, (err, address, family)=>{
+      // If valid url hostname is given in box
       if(address){
-        res.json({original_url: urlResult});
+        Url_data.findOne({original_url: urlResult}, (err, data)=>{
+          if(err){
+            console.log(err);
+          }
+          else{
+            // If there is same url is not present in database
+            if(!data){
+              Url_data.find({short_url: {$gte: 0}}, (err, data)=>{
+                if(err){
+                  console.log(err);
+                }
+                else{
+                  indexOfShortUrl = data[data.length - 1].short_url;
+                  indexOfShortUrl += 1;
+                  UrlNew = new Url_data({
+                    original_url: urlResult,
+                    short_url: indexOfShortUrl
+                  });
+                  UrlNew.save();
+                }
+                res.json({original_url: urlResult, short_url: indexOfShortUrl});
+              });
+            }
+            // If there is same url is present in database
+            else{
+              indexOfShortUrl = data.short_url;
+              UrlNew = new Url_data({
+                original_url: urlResult,
+                short_url: indexOfShortUrl
+              });
+              res.json({original_url: urlResult, short_url: indexOfShortUrl});
+            }
+          }
+        });
       }
+      // If invalid url hostname is given in box
       else{
         res.json({error: "Invalid hostname"});
       }
     });
   }
+  // If there is https:// or http:// missing in url
   else{
     res.json({error: "Invalid url"});
   }
 });
 
+app.get("/api/shorturl/:id", (req, res)=>{
+  indexOfShortUrl = req.params.id;
+  Url_data.findOne({short_url: indexOfShortUrl}, (err, data)=>{
+    if(err){
+      res.json({message: "There is no such a short url saved in database"})
+    }
+    else{
+      res.redirect(data.original_url);
+      res.end();
+    }
+  });
+});
 
 
 app.listen(port, function() {
